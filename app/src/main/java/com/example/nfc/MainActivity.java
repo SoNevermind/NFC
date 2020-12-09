4b6b56dfcce84e51a45df78580275f0587354f1a
Nevermind <1276411@bsu.edu.ru>
2020-12-09 03:45:32 +0300

    Переписанный манифест, переделанная логика считывания

--- app/src/main/java/com/example/nfc/MainActivity.java
+++ app/src/main/java/com/example/nfc/MainActivity.java
@@ -1,232 +1,122 @@
 package com.example.nfc;
 
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.NfcA;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String TAG = "MainActivity";
-    private TextView tagId;
-    private ImageView mImageView;
-    private boolean NFCEnabled;
-    private String WriteString;
-    private String CurAction;
-    private int WritePGNum;
+    public static final String errorDetected = "NFC метка не обнаружена";
+    public static final String writeSuccess = "Успешное считывание!";
+    public static final String writeError = "Ошибка при записи, попробуйте снова";
+
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter intentFilter[];
+    boolean writeMode;
+    Tag tag;
+    Context context;
+
+    TextView editMessage, nfcContents;
+    Button activateButton;
 
     DBHelper dbHelper;
 
-    public static String unHex(String arg){
-        StringBuilder stringBuilder = new StringBuilder();
+    private NdefRecord createRecord(String text) throws UnsupportedEncodingException{
+        String lang = "ru";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("UTF-8");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+        byte[] payload = new byte[1 + langLength + textLength];
 
-        for(int i = 0; i < arg.length(); i += 2){
-            String s = arg.substring(i, (i + 2));
+        //установка байт состояний
+        payload[0] = (byte) langLength;
 
-            int decimal = Integer.parseInt(s, 16);
-            stringBuilder.append((char) decimal);
-        }
+        //копирование длины байтов и текстовых байтов в payload
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(langBytes, 0, payload, 1 + langLength, textLength);
 
-        return stringBuilder.toString();
+        NdefRecord record = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+
+        return record;
     }
 
-    public void sendResult(String Id, String DataRfId, String[] TechList){
-        Intent intent = new Intent();
+    private void write(String text, Tag tag) throws IOException, FormatException{
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage message = new NdefMessage(records);
 
-        if(DataRfId.isEmpty()){
-            DataRfId = "empty";
-        }
+        //получение экземпляра Ndef для метки
+        Ndef ndef = Ndef.get(tag);
 
-        intent.putExtra("tech", TechList);
-        intent.putExtra("result", DataRfId);
+        //включить ввод / вывод
+        ndef.connect();
 
-        if(CurAction.equals("read") && !DataRfId.equals("empty")){
-            intent.putExtra("text", unHex(DataRfId));
-        } else{
-            intent.putExtra("text", DataRfId);
-        }
+        //написание сообщения
+        ndef.writeNdefMessage(message);
 
-        intent.putExtra("event", CurAction);
-        intent.putExtra("uid", Id);
-        setResult(RESULT_OK, intent);
-        finish();
+        //закрытие соединения
+        ndef.close();
     }
 
-    public static String ByteArrayToHexString(byte[] bytes){
-        final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        char[] hexChars = new char[bytes.length * 2];
-        int v;
-
-        for(int i = 0; i < bytes.length; i++){
-            v = bytes[i] & 0xFF;
-            hexChars[i * 2] = hexArray[v >>> 4];
-            hexChars[i * 2 + 1] = hexArray[v & 0x0F];
-        }
-
-        return new String(hexChars);
-    }
-
-
-    @Override
-    protected void onNewIntent(Intent intent){
-        StringBuilder DataRfId = new StringBuilder();
-        int pgcount;
-        String Id = "";
-        String[] TechList;
-
-        NfcA nfcA = null;
-
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-        if(tag == null){
-            sendResult(Id, DataRfId.toString(), null);
+    private void buildTadViews(NdefMessage[] messages){
+        if(messages == null || messages.length == 0){
             return;
         }
 
-        Id = ByteArrayToHexString(tag.getId());
-        TechList = tag.getTechList();
+        String text = "";
+        byte[] payload = messages[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; //получение текстовой кодировки
+        int languageCodeLength = payload[0] & 0063; //получение языковой кодировки
 
         try{
-            nfcA = NfcA.get(tag);
-            if(nfcA == null){
-                sendResult(Id, DataRfId.toString(), TechList);
-                return;
-            }
-
-            nfcA.connect();
-
-            if(!nfcA.isConnected()){
-                sendResult(Id, DataRfId.toString(), TechList);
-                return;
-            }
-
-            if(CurAction.equals("write")){
-                int ind = 0;
-                int writeLengtht = WriteString.length();
-                String writeStr;
-
-                for(int pageNum = WritePGNum; pageNum <= nfcA.getMaxTransceiveLength() - 2 && ind < writeLengtht; pageNum++){
-                    byte[] PG = new byte[]{(byte) 0x000, (byte) 0x000, (byte) 0x000, (byte) 0x000};
-
-                    if(writeLengtht < ind + 4){
-                        writeStr = WriteString.substring(ind, writeLengtht);
-
-                        if(writeStr.length() >= 1){
-                            PG[0] = (byte) ((int) writeStr.charAt(0) & 0x0ff);
-                        }
-                        if(writeStr.length() >= 2){
-                            PG[1] = (byte) ((int) writeStr.charAt(1) & 0x0ff);
-                        }
-                        if(writeStr.length() >= 3){
-                            PG[2] = (byte) ((int) writeStr.charAt(2) & 0x0ff);
-                        }
-                    } else {
-                        writeStr = WriteString.substring(ind, ind + 4);
-
-                        for(int i = 0; i < 3; i++){
-                            PG[i] = (byte) ((int) writeStr.charAt(i) & 0x0ff);
-                        }
-                    }
-
-                    byte[] result = nfcA.transceive(new byte[]{
-                            (byte) 0xA2,
-                            (byte) (pageNum & 0x0ff),
-                            PG[0],
-                            PG[1],
-                            PG[2],
-                            PG[3]
-                    });
-
-                    if(result == null){
-                        DataRfId.append(", error");
-                    } else if((result.length == 1) && ((result[0] & 0x00A) != 0x00A)){
-                        DataRfId.append(ByteArrayToHexString(result));
-                    } else{
-                        DataRfId.append(", success");
-                    }
-
-                    ind += 4;
-                }
-            } else{
-                pgcount = nfcA.getMaxTransceiveLength() / 4;
-
-                for(int pageNum = 0; pageNum < pgcount; pageNum += 4){
-                    byte[] result = nfcA.transceive(new byte[]{
-                            (byte) 0x30,
-                            (byte) (pageNum & 0x0ff)
-                    });
-
-                    DataRfId.append(ByteArrayToHexString(result));
-                }
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        } finally {
-            if(nfcA != null){
-                try{
-                    nfcA.close();
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onPause(){
-        super.onPause();
-
-        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
-
-        if(nfc != null && NFCEnabled){
-            nfc.disableForegroundDispatch(this);
-            NFCEnabled = false;
+            //получение текста
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e){
+            Log.e("UnsupportedEncoding", e.toString());
         }
 
-        finish();
+        nfcContents.setText("Содержимое NFC: " + text);
     }
 
-    @Override
-    public void onResume(){
-        super.onResume();
+    private void readFromIntent(Intent intent){
+        String action = intent.getAction();
 
-        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)){
+            Parcelable[] rawMessage = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] messages = null;
 
-        if(nfc != null){
-            final Intent intent = new Intent(this.getApplicationContext(), this.getClass());
+            if(rawMessage != null){
+                messages = new NdefMessage[rawMessage.length];
 
-            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+                for(int i = 0; i < rawMessage.length; i++){
+                    messages[i] = (NdefMessage) rawMessage[i];
+                }
+            }
 
-            String[][] techList = new String[3][1];
-
-            techList[0][0] = "android.nfc.tech.NdefFormatable";
-            techList[1][0] = "android.nfc.tech.NfcA";
-            techList[2][0] = "android.nfc.tech.Ndef";
-
-            IntentFilter[] filters = new IntentFilter[2];
-
-            filters[0] = new IntentFilter();
-            filters[0].addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
-            filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-            filters[1] = new IntentFilter();
-            filters[1].addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
-            filters[1].addCategory(Intent.CATEGORY_DEFAULT);
-
-            nfc.enableForegroundDispatch(this, pendingIntent, filters, techList);
-
-            NFCEnabled = true;
+            buildTadViews(messages);
         }
     }
 
@@ -235,35 +125,117 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new DBHelper(this);
+        editMessage = (TextView) findViewById(R.id.editMessage);
+        activateButton = (Button) findViewById(R.id.activateButton);
+        nfcContents = (TextView) findViewById(R.id.nfcContents);
 
-        //TODO дописать главный метод установки параметров и привязать к БД
+        context = this;
+
+        activateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    //при необнаружении NFC метки
+                    if(tag == null){
+                        Toast.makeText(context, errorDetected, Toast.LENGTH_LONG).show();
+                    } else {
+                        //при успешном обнаружении
+                        write("Просканируйте NFC метку: " + editMessage.getText().toString(), tag);
+                        Toast.makeText(context, writeSuccess, Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e){
+                    Toast.makeText(context, writeError, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                } catch (FormatException e){
+                    Toast.makeText(context, writeError, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        //проверка на подходящее устройство
+        if(nfcAdapter == null){
+            Toast.makeText(this, "Данное устройство не поддерживает NFC", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        readFromIntent(getIntent());
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+
+        intentFilter = new IntentFilter[] { tagDetected };
     }
 
     @Override
     public void onClick(View view){
         //Cчитываем знаечение полей и сохраняем в эти переменные
-        String productName = product_name.getText().toString();
-        String productId = product_id.getText().toString();
+        String productId = tag.getId().toString();
 
         SQLiteDatabase database = dbHelper.getWritableDatabase(); //Создается и обновляется новая таблица, если нет существующей
 
         //для добавления новых строк в таблицу
         ContentValues values = new ContentValues();
 
-        //Разделяем действия по отдельным кнопкам
-        switch (view.getId()){
-            case R.id.add:
-                values.put(DBHelper.KEY_PRODUCT_NAME, productName);
-                values.put(DBHelper.KEY_PRODUCT_ID, productId);
+        values.put(DBHelper.KEY_PRODUCT_ID, productId);
+        database.insert(DBHelper.TABLE_CONTACTS, null, values);
 
-                database.insert(DBHelper.TABLE_CONTACTS, null, values);
-                break;
-            case R.id.delete:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                break;
+        //считываем данные бд в консоль
+        Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+
+        if(cursor.moveToFirst()){
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int idIndex2 = cursor.getColumnIndex(DBHelper.KEY_PRODUCT_ID);
+
+            do{
+                Log.d("mLog", "ID = " + cursor.getInt(idIndex) + ", Product_ID = " + cursor.getInt(idIndex2));
+            } while (cursor.moveToNext());
+        } else {
+            Log.d("mLog", "0 rows");
         }
 
+        cursor.close();
+
         dbHelper.close();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+
+        setIntent(intent);
+        readFromIntent(intent);
+
+        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
+            tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        writeModeOff();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        writeModeOn();
+    }
+
+    //разрешить запись
+    private void writeModeOn(){
+        writeMode = true;
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+    }
+
+    //отклонить запись
+    private void writeModeOff(){
+        writeMode = false;
+        nfcAdapter.disableForegroundDispatch(this);
     }
 }
